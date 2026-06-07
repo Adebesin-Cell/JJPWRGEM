1829f097f8a3fa6b23535ed3bab7800cd4fa6f6a
Jacob Asper <jacobasper191@gmail.com>
2026-06-06 21:35:37 -0400

    chore: update launch settings for vscode development

--- npm-packages/jjpwrgem-vscode/.vscode-test.mjs
+++ npm-packages/jjpwrgem-vscode/.vscode-test.mjs
@@ -2,7 +2,13 @@ import { defineConfig } from "@vscode/test-cli";
 
 export default defineConfig({
   files: "out/test/**/*.test.js",
-  launchArgs: ["--skip-welcome", "--disable-extensions", "--skip-release-notes"],
+  launchArgs: [
+    "--skip-welcome",
+    "--disable-extensions",
+    "--disable-extension",
+    "vscode.json-language-features",
+    "--skip-release-notes",
+  ],
   settings: {
     "editor.defaultFormatter": "20jasper.jjpwrgem-vscode",
   },
